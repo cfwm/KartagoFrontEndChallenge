3eeb47bcd95e166b63b928c38f7675f77e017c3b
Carlos Moreira <carlosfwm@gmail.com>
2020-05-21 16:42:44 -0300

    tutorial inicial e primeiros ajustes exploratrórios.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Admin, Resource } from 'react-admin';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+import jsonServerProvider from 'ra-data-json-server';
+
+import { createMuiTheme } from '@material-ui/core/styles';
+import purple from '@material-ui/core/colors/purple';
+import green from '@material-ui/core/colors/green';
+
+import PostIcon from '@material-ui/icons/Book';
+import UserIcon from '@material-ui/icons/Group';
+
+import Dashboard from './Dashboard';
+import NotFound from './NotFound';
+
+import authProvider from './authProvider'
+
+
+import { UserList } from './users';
+import { PostCreate, PostList, PostEdit } from './posts';
+import { TodoList, TodoEdit, TodoCreate } from './todos';
+
+const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: purple,
+    secondary: green, 
+  }
+});
+
+const App = () => (
+  <Admin theme={theme} dashboard={Dashboard} authProvider={authProvider} dataProvider={dataProvider} catchAll={NotFound}>
+    <Resource name="users" list={UserList} icon={UserIcon} />
+    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
+    <Resource name="todos" list={TodoList} edit={TodoEdit} create={TodoCreate} />
+  </Admin>
+);
 export default App;
